f574e555da7fee8c261b0336e99062d1418ee499
zgualberto <zngualberto@yahoo.com>
2020-10-22 19:48:58 +0800

    initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->increments()->unsigned();
+            $table->string('prefixname', 255)->nullable();
+            $table->string('firstname', 255);
+            $table->string('middlename', 255)->nullable();
+            $table->string('lastname', 255);
+            $table->string('suffixname', 255)->nullable();
+            $table->string('username', 255)->unique();
+            $table->string('email')->unique();
+            $table->text('password');
+            $table->text('photo');
+            $table->string('type', 255)->default('user');
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+            $table->softDeletesTz('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
